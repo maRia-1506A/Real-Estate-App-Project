430b8618157d66636153e962d1a15dd306298e16
maRia-1506A <zannatulfarzana363@gmail.com>
2025-11-22 01:44:03 +0600

    header section

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,83 +1,38 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import { Plus } from 'lucide-react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 function Header() {
+  const path = usePathname();
+
+  useEffect(() => {
+    console.log(path)
+  }, [])
   return (
-    <div>
-      <div className='navbar1'>
-        <Image src={'/logo.svg'} height={150} width={150} alt='logo' />
-        <Image src={'/logo.svg'} height={150} width={150} alt='logo' />
-        <Button><Plus />Post Your Ad</Button>
-        <Button><Plus />Post Your Ad</Button>
+    <div className='p-6 px-10 flex justify-between shadow-sm fixed top-0 w-full z-10 bg-white'>
+      <div className='flex gap-12 items-center'>
+        <Image src={'/logo.svg'} height={150} width={150}
+          alt='logo' />
+        <ul className='hidden md:flex gap-10'>
+          <Link href={'/'}>
+            <li className={`'hover:text-primary font-medium text-sm cursor-pointer'
+              ${path == '/'}&& text-primary`}>For Sale</li>
+          </Link>
+          <li className='hover:text-primary font-medium text-sm cursor-pointer'>For Rent</li>
+          <li className='hover:text-primary font-medium text-sm cursor-pointer'>Agent Finder</li>
+        </ul>
+      </div>
+
+      <div className='flex gap-2'>
+        <Button className='flex gap-2'><Plus />Post Your Ad</Button>
+        <Button variant="outline">Login</Button>
       </div>
     </div>
-
-    
   )
 }
 
 export default Header;
-
-// import React from 'react'
-// import Image from 'next/image'
-
-// function Header() {
-//   return (
-//     <header className="bg-white shadow-sm">
-//       <div className="max-w-120xl mx-auto px-4 py-3 flex items-center justify-between">
-        
-//         {/* Left: Logo */}
-//         <Image src="/logo.svg" width={1200} height={1200} alt="logo" />
-
-//         {/* Right: Buy Now */}
-//         <button className="preview__action--buy header-buy-now e-btn--3d -color-primary px-4 py-2 rounded-md">
-//           Buy now
-//         </button>
-
-//       </div>
-//     </header>
-//   )
-// }
-
-// export default Header
-
-// export default function Header() {
-//   return (
-//     <header className="bg-white shadow-sm">
-//       <div className="max-w-120xl mx-auto px-4 py-4 flex items-center justify-between">
-//         <div className="font-bold text-xl text-indigo-12000">CozyList</div>
-
-//         <nav className="flex items-center gap-4">
-//           <div className="px-3 py-1 border rounded">Create</div>
-//           <div className="px-3 py-1 border rounded">Favourites</div>
-//           <div className="px-3 py-1 border rounded">Login</div>
-//         </nav>
-//       </div>
-//     </header>
-//   )
-// }
-
-
-
-// import React from 'react'
-// import Image from 'next/image'
-
-// function Header() {
-//   return (
-//     <header className="bg-white shadow-sm">
-//       <div className="max-w-120xl mx-auto px-4 py-4 flex items-center gap-3 text-xl font-semibold">
-//         <Image src="/logo.svg" width={24} height={24} alt="logo" />
-//         Our Home
-//       </div>
-//     </header>
-//   )
-// }
-
-// export default Header
-
-
-
-
-
